3fc7aa1d9700a67007b749f675074f68ff294c56
Pooja <p1219513@gmail.com>
2024-12-08 21:34:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'your_secret_key'
 Bootstrap5(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
@@ -61,7 +61,7 @@ def home():
 @app.route("/add",methods = ["GET","POST"])
 def add_movie():
     form = AddMovie()
-    api_key = "9fc65133"
+    api_key = "your_authorized_api_key"
     movie_title = form.movie_name.data
     url = f"http://www.omdbapi.com/?apikey={api_key}&t={movie_title}"
 
